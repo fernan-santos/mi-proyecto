e88ed524e3d4e5b4ecc3b8bdaa8386d0cc9e46cd
fernan-santos <75287002@continental.edu.pe>
2026-02-14 06:33:29 -0500

    Update CartaWaWaWa.jsx

--- src/CartaWaWaWa.jsx
+++ src/CartaWaWaWa.jsx
@@ -63,16 +63,7 @@ export default function CartaWaWaWa() {
         <source src="/aud.mp3" type="audio/mpeg" />
       </audio>
       
-      {/* GIF izquierdo */}
-      <div className="gif-left">
-          <video src="/pepelove.mp4" alt="video/mp4" />
-        </div>
 
-      {/* GIF derecho */}
-      <div className="gif-right">
-        <video src="/pepelove.mp4" alt="video/mp4" />
-      </div>
-      
       <div
         className={`envelope ${opened ? 'opened' : ''}`}
         onClick={handleEnvelopeClick}
