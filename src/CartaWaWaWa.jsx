d980099724d7b3a941312957007a34d962a44ae6
fernan-santos <75287002@continental.edu.pe>
2026-02-14 05:19:24 -0500

    commit 2

--- src/CartaWaWaWa.jsx
+++ src/CartaWaWaWa.jsx
@@ -65,12 +65,12 @@ export default function CartaWaWaWa() {
 
       {/* GIF izquierdo */}
       <div className="gif-left">
-        <img src="/pepelove.mp4" alt="decoración" />
+        <video src="/pepelove.mp4" alt="video/mp4" />
       </div>
 
       {/* GIF derecho */}
       <div className="gif-right">
-        <img src="/pepelove.mp4" alt="decoración" />
+        <video src="/pepelove.mp4" alt="video/mp4" />
       </div>
 
       <div
